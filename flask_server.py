27fcec727c9009783d7e45082a8ef0f3e5bc85e1
Haider ali <88321432+HksH143@users.noreply.github.com>
2022-02-13 22:41:59 +0500

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -84,13 +84,14 @@ def receive_file():
 
         for file in files:
             if file != '.gitignore':
-                file_size = os.path.getsize(file) 
+                file_dir=save_path+"/"+file 
+                file_size = os.path.getsize(file_dir) 
                 print("File: ",file," Size: ",file_size/1000000, 'mb'," bytes : ",file_size)
 
         
 
         # return redirect(request.url)
-        return "server has received the file "
+        return "server has received the image "
     # return "server has received name: "
 
 
